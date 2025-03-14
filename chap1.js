5e929e6b0e56d0acae2ec60cddd3b57aa55740f5
Vedikapant <vedikapant07@gmail.com>
2025-03-15 00:07:33 +0530

    Chapter1-Operators

---
+++ chap1.js
@@ -0,0 +1,16 @@
+//print hello world
+console.log("Hello World");
+
+console.log(10 + 20);
+
+//integer subtraction
+console.log(10 - 8);
+
+console.log(10 * 20);
+
+console.log(10 / 20);
+
+console.log(10 + 15 * 20);
+
+console.log((10 + 5) * 8);
+
